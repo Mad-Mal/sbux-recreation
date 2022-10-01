4f9af6bfa9e88eabe96b85ef58a0f514e8365401
Mallory <mallorydoescoding@gmail.com>
2022-10-01 13:16:16 -0500

    Tweaked a few thinngs in the getStarted section to mesh well with this component, got all files setup for this component.

---
+++ sbux-recreation/src/components/bottomNav.js
@@ -0,0 +1,15 @@
+//Main imports
+import React from 'react';
+
+//Style imports
+import '../styles/bottomNav.css';
+
+const bottomNav = () => {
+  return (
+    <div className='bottomNav_wrap'>
+
+    </div>
+  )
+}
+
+export default bottomNav
