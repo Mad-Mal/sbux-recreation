3cce137d2cd614114d34a7e71205c61aa26375b2
Mallory <mallorydoescoding@gmail.com>
2022-10-18 13:50:38 -0500

    navbar is complete and styled except for the logos for the buttons and the onClick.

--- sbux-recreation/src/components/bottomNav.js
+++ sbux-recreation/src/components/bottomNav.js
@@ -7,7 +7,26 @@ import '../styles/bottomNav.css';
 const bottomNav = () => {
   return (
     <div className='bottomNav_wrap'>
-
+        <div>
+            <img />
+            <p>Home</p>
+        </div>
+        <div>
+            <img />
+            <p>Scan</p>
+        </div>
+        <div>
+            <img />
+            <p>Order</p>
+        </div>
+        <div>
+            <img />
+            <p>Gift</p>
+        </div>
+        <div>
+            <img />
+            <p>Offers</p>
+        </div>
     </div>
   )
 }
