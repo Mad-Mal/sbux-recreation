e88ddd3b050bacd8d0719413a5708a9b67ab8a4f
Mallory <mallorydoescoding@gmail.com>
2022-10-20 13:47:46 -0500

    Finished the bottom navBar, all it lacks now is functionallity.

--- sbux-recreation/src/components/bottomNav.js
+++ sbux-recreation/src/components/bottomNav.js
@@ -4,29 +4,40 @@ import React from 'react';
 //Style imports
 import '../styles/bottomNav.css';
 
+//Settup for Emojis
+let homeEmoji = String.fromCodePoint(0x1f3e0)
+
+let scanEmoji = '\u2728'
+
+let orderEmoji = String.fromCodePoint(0x1f4f2)
+
+let giftEmoji = String.fromCodePoint(0x1f381)
+
+let offerEmoji = String.fromCodePoint(0x1f911)
+
 const bottomNav = () => {
   return (
     <div className='bottomNav_wrap'>
-        <div>
-            <img />
+        <button>
+            <p>{homeEmoji}</p>
             <p>Home</p>
-        </div>
-        <div>
-            <img />
+        </button>
+        <button>
+            <p>{scanEmoji}</p>
             <p>Scan</p>
-        </div>
-        <div>
-            <img />
+        </button>
+        <button>
+            <p>{orderEmoji}</p>
             <p>Order</p>
-        </div>
-        <div>
-            <img />
+        </button>
+        <button>
+            <p>{giftEmoji}</p>
             <p>Gift</p>
-        </div>
-        <div>
-            <img />
+        </button>
+        <button>
+            <p>{offerEmoji}</p>
             <p>Offers</p>
-        </div>
+        </button>
     </div>
   )
 }
